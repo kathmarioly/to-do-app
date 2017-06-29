cb3b71142c3479b51333a5248ae681462df01236
kathmarioly <katerinemarioly@gmail.com>
2017-06-28 23:16:48 -0400

    Add Task

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("button").click(function(e){
+		e.preventDefault();
+		var name = $("#nombre").val();
+		var message = $("#mensaje").val();
+		if(message == ""){
+			alert("Debes escribir tu nombre y el mensaje");
+			}else{
+			$('#comentarios').append('<div</span><p class="center-align">'+message+'</p></div>');
+			}
+	});
+});
